fdfcd896361a65d611ae81a40158c8a83a01b2a2
Pakornpat Amornlertpanit <pakornpat.a@gmail.com>
2022-05-29 15:40:08 +0700

    Submit H/W 1 day24

--- Day24/day24-homework1/src/App.js
+++ Day24/day24-homework1/src/App.js
@@ -1,15 +1,11 @@
-import './App.css';
-import {useState, useRef, useEffect} from 'react'
+import "./App.css";
+ // eslint-disable-next-line 
+import { useState, useRef, useEffect } from "react";
 
 function Form(props) {
-  let firstname = useRef()
-  let lastname = useRef()
-  let gender = useRef()
-
-  useEffect(() => {
-    console.log('Multi useEffect gender', gender)
-    }, [gender]);
-   
+  let firstname = useRef();
+  let lastname = useRef();
+  let gender = useRef();
 
   const submitForm = () => {
     // console.log(firstname.current.value)
@@ -17,55 +13,59 @@ function Form(props) {
     // console.log(gender.current.value)
 
     props.getValue({
-      name: firstname.current.value, 
+      name: firstname.current.value,
       lastname: lastname.current.value,
-      gender: gender.current.value
-    })
-  }
+      gender: gender.current.value,
+    });
+  };
 
   return (
     <>
+      <h1> Form List</h1>
       <input type="text" ref={firstname} />
       <input type="text" ref={lastname} />
 
       <select ref={gender}>
         <option value="Male">Male</option>
         <option value="Female">Female</option>
-        <option value="Na">NA</option>
+        <option value="NA">NA</option>
       </select>
 
-      <input type="button" value="submit" onClick={submitForm}/>
-    </> 
-  )
+      <input type="button" value="submit" onClick={submitForm} />
+    </>
+  );
 }
 
-
 function App() {
-
   //useState
-  const [MaleList, setMaleList] = useState([])
-  const [FemaleList, setFemaleList] = useState([])
-  const [NaList, setNaList] = useState([])
-
+  const [MaleList, setMaleList] = useState([]);
+  const [FemaleList, setFemaleList] = useState([]);
+  const [NaList, setNaList] = useState([]);
 
   //getValue
   const getValue = (value) => {
-    console.log(value)
-  }
+    if (value.gender === "Male") {
+      return setMaleList([...MaleList, value]);
+    } else if (value.gender === "Female") {
+      return setFemaleList([...FemaleList, value]);
+    } else {
+      return setNaList([...NaList, value])
+    }
+  };
   return (
     <>
-    <h1> Form List</h1>
-    <Form getValue={getValue}/>
-
-    <hr/>
-    <h3>Male List</h3>
+      <Form getValue={getValue} />
+      <hr />
+      <h3>Male List</h3>
       <p>{JSON.stringify(MaleList)}</p>
-    <h3>Female List</h3>
+      <p>{MaleList.length}</p>
+      <h3>Female List</h3>
       <p>{JSON.stringify(FemaleList)}</p>
-    <h3>NA List</h3>
-      <p>{JSON.stringify(NaList)}</p>
+      <p>{FemaleList.length}</p>
+      <h3>NA List</h3>
+      <p>{JSON.stringify(NaList)}</p> 
+      <p>{NaList.length}</p>
     </>
-    
   );
 }
 
